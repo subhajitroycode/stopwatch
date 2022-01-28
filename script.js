e5bcb67cf8e04b5b1f6e02a5157b5899de67609b
Subhajit Roy <roysubhajit8697@gmail.com>
2022-01-28 19:53:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let intervalID = 0;
+const secTime = document.getElementById("seconds");
+const minTime = document.getElementById("minutes");
+const hourTime = document.getElementById("hours");
+
+const cursor = (id, value) => {
+    id.style.cursor = value;
+}
+
+const startTime = document.getElementById('startButton');
+startTime.onclick = () => {
+    intervalID = window.setInterval(stopwatch, 1000);
+    
+    startTime.disabled = true;
+    stopTime.disabled = false;
+    resetTime.disabled = false;
+
+    cursor(stopTime, "pointer");
+    cursor(resetTime, "pointer");
+}
+
+const stopwatch = () => {
+    seconds++;
+    
+    if(seconds < 10)
+        secTime.innerText = `0${seconds}`;
+    else
+        secTime.innerText = seconds;
+
+    if(seconds === 60){
+        minutes++;
+        
+        if(minutes < 10)
+            minTime.innerText = `0${minutes}`;
+        else
+            minTime.innerText = minutes;
+        
+        seconds = 0;
+        secTime.innerText = `0${seconds}`;
+    }
+
+    if(minutes === 60) {
+        hours++;
+
+        if(hours < 10)
+            hourTime.innerText = `0${hours}`;
+        else
+            hourTime.innerText = hours;
+
+        minutes = 0;
+        seconds = 0;
+        minTime.innerText = `0${minutes}`;
+        secTime.innerText = `0${seconds}`;
+    }
+    
+}
+
+const stopTime = document.getElementById('stopButton');
+stopTime.onclick = () => {
+    window.clearInterval(intervalID);
+    startTime.disabled = false;
+}
+
+const resetTime = document.getElementById('resetButton');
+resetTime.onclick = () => {
+    clearInterval(intervalID);
+    startTime.disabled = false;
+    seconds = 0;
+    hourTime.innerText = '00';
+    minTime.innerText = '00';
+    secTime.innerText = '00';
+}
